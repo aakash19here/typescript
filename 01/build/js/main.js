d0c14322d62e133a617d8c449b028f7fbd7d683d
aakash19here <spacex3nasa@gmail.com>
2023-04-29 20:25:07 +0530

    01

---
+++ 01/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 12;
+// let b : number = "12";
+let c = 1;
+// console.log(a/b);
+console.log(a * c);
---
+++ 01/src/main.ts
@@ -0,0 +1,6 @@
+let a : number = 12;
+// let b : number = "12";
+let c : number = 1
+
+// console.log(a/b);
+console.log(a * c);
